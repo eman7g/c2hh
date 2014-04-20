ef3e3e75066e8d041cc3e43b17faf354b599f2d8
Emmanuel Garcia <eman.7g@gmail.com>
2014-04-20 10:58:41 -0700

    Updated blog landing page

--- base.php
+++ base.php
@@ -13,7 +13,21 @@
     }
   ?>
 
+
+
   <div class="wrap container" role="document">
+
+    <?php if(is_home() || is_archive()) : ?>
+      
+      <div class="page-header">
+        <h1 class="section-title">
+          <?php echo roots_title(); ?>
+        </h1>
+        <hr class="title-divider">
+      </div>
+
+    <?php endif ?>
+     
     <div class="content row">
     
       <div class="main <?php echo roots_main_class(); ?>" role="main">
