d5051d726bf3fd664ecc9a39bdc1d5359d8ef700
Emmanuel Garcia <eman.7g@gmail.com>
2014-04-28 00:08:20 -0700

    Updated cart pages

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/app.css', false, 'd96b9882801e7ef699828f8d0872785a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/app.css', false, 'de25287bb43a1130d0d8d9100075013e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
