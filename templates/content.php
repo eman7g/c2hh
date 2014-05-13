32627ea30b3bf9393107aacc77cff78a77e33a7e
Emmanuel Garcia <eman.7g@gmail.com>
2014-05-13 13:29:20 -0700

    Updated blog and footer styles

--- templates/content.php
+++ templates/content.php
@@ -3,6 +3,18 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
+  <div class="entry-feature-img">
+  <?php 
+    if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+      the_post_thumbnail();
+      if (get_post(get_post_thumbnail_id())->post_excerpt){
+        echo "<small>";
+        echo get_post(get_post_thumbnail_id())->post_excerpt;
+        echo "</small>";
+      }
+    } 
+  ?>
+</div>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
   </div>
