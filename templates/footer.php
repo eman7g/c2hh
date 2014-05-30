756f718d58c42f35087fd82e23be7bcbc117abbb
Emmanuel Garcia <eman.7g@gmail.com>
2014-05-29 21:02:25 -0700

    Updated footer

--- templates/footer.php
+++ templates/footer.php
@@ -3,11 +3,26 @@
 		<div class="row">
 			
 			<div class="col-sm-4 col-footer">
-				<?php wp_nav_menu( array('menu' => 'Footer Menu 1' )); ?>
+				<?php wp_nav_menu( array('menu' => 'Footer Menu' )); ?>
 			</div>
 
 			<div class="col-sm-4  col-footer">
-				<?php wp_nav_menu( array('menu' => 'Footer Menu 2' )); ?>				
+				<h3>Product Types</h3>
+				<?php
+					$terms = get_terms("product-type");
+					 if ( !empty( $terms ) && !is_wp_error( $terms ) ){
+					     echo "<ul>";
+					     foreach ( $terms as $term ) {
+					       //echo "<li>" . $term->name . "</li>";
+					       ?>
+					       	<li>
+					       		<a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Products' ) ) ); ?>?product_type=<?php echo $term->slug;?>"><?php echo $term->name;?></a></li>
+					       <?php
+					        
+					     }
+					     echo "</ul>";
+					 }
+				?>
 			</div>
 
 			<div class="col-sm-4 col-footer">
