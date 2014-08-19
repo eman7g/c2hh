7a76d0817f27a8177fe9c8d2bc10b23fb359e532
Emmanuel Garcia <eman.7g@gmail.com>
2014-08-19 10:25:19 -0700

    Updated favicon

--- templates/head.php
+++ templates/head.php
@@ -7,6 +7,8 @@
   <meta charset="utf-8">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
   <?php wp_head(); ?>
 
